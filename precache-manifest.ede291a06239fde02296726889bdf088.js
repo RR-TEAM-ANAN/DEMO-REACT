f286b6b421c67c04054602d7c0e8c6065b8cd398
anankranpoon@gmail.com <anankranpoon@gmail.com>
2019-12-25 15:16:39 +0700

    Updates

---
+++ precache-manifest.ede291a06239fde02296726889bdf088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231e58d300f3cf5f29dedc1c7862b94f",
+    "url": "/DEMO-REACT/index.html"
+  },
+  {
+    "revision": "aa06988c0ad601f024d9",
+    "url": "/DEMO-REACT/static/js/2.5eb0162e.chunk.js"
+  },
+  {
+    "revision": "ed8dc1f2ac82f05050a6c0f07a6d93ef",
+    "url": "/DEMO-REACT/static/js/2.5eb0162e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d319bbbf2043728ae817",
+    "url": "/DEMO-REACT/static/js/main.f2709a6f.chunk.js"
+  },
+  {
+    "revision": "212d515d991d876cf12f",
+    "url": "/DEMO-REACT/static/js/runtime-main.830a7a54.js"
+  },
+  {
+    "revision": "6c2104b8d219ed99234ae2d6329f4357",
+    "url": "/DEMO-REACT/static/media/index.6c2104b8.less"
+  }
+]);
